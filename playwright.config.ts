f9dfe8ac13b8e00f42780579ca2fdef365f59513
hj-ueda <natsuki.ueda@headjapan.com>
2024-02-15 12:53:43 +0900

    並列数をenvに記載

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: process.env.WORKERS ?? 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
